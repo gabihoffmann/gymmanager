a80af9384d105414012561e073f4ad1ecefb079a
Gabi Hoffmann <hoffmann.agf@gmail.com>
2021-02-07 16:21:13 -0300

    :recycle: restarting learning. Launchbase module 04 - Controle de academia

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,26 +1,29 @@
-const express = require('express')
-const routes = express.Router()
-const instructors = require('../app/controllers/instructors')
-const members  = require('../app/controllers/members')
+const express = require("express");
+const routes = express.Router();
 
-routes.get('/', (req,res) => {
-    res.redirect('/instructors')
-})
+// const instructors = require('../app/controllers/instructors')
+// const members  = require('../app/controllers/members')
+
+routes.get("/", (req, res) => {
+  res.redirect("/instructors");
+});
 //Instructors
-routes.get('/instructors', instructors.index)
-routes.get('/instructors/create', instructors.create)
-routes.get('/instructors/:id', instructors.show)
-routes.get('/instructors/:id/edit', instructors.edit)
-routes.post('/instructors', instructors.post)
-routes.put('/instructors', instructors.put)
-routes.delete('/instructors', instructors.delete)
+routes.get("/instructors", (req, res) => {
+  res.render("instructors/index");
+});
+//routes.get("/instructors/create", instructors.create);
+// routes.get('/instructors/:id', instructors.show)
+// routes.get('/instructors/:id/edit', instructors.edit)
+// routes.post('/instructors', instructors.post)
+// routes.put('/instructors', instructors.put)
+// routes.delete('/instructors', instructors.delete)
 //Members
-routes.get('/members', members.index)
-routes.get('/members/create', members.create)
-routes.get('/members/:id', members.show)
-routes.get('/members/:id/edit', members.edit)
-routes.post('/members', members.post)
-routes.put('/members', members.put)
-routes.delete('/members', members.delete)
+routes.get("/members", (req, res) => res.render("members/index"));
+// routes.get('/members/create', members.create)
+// routes.get('/members/:id', members.show)
+// routes.get('/members/:id/edit', members.edit)
+// routes.post('/members', members.post)
+// routes.put('/members', members.put)
+// routes.delete('/members', members.delete)
 
-module.exports = routes
+module.exports = routes;
--- src/server.js
+++ src/server.js
@@ -1,24 +1,23 @@
-const express = require('express')
-const nunjucks = require('nunjucks')
-const routes = require('./routes/routes')
-const methodOverride = require('method-override')
+const path = require("path");
+const express = require("express");
+const nunjucks = require("nunjucks");
+// const methodOverride = require("method-override");
+const routes = require(path.resolve(__dirname, "routes", "routes"));
 
-const server = express()
+const server = express();
 // habilitando o body request de qualquer tipo
-server.use(express.urlencoded({extended:true}))
-server.use(express.static('public'))
-//sobrescrita de metodo http 
-server.use(methodOverride('_method'))
-server.use(routes)
+// server.use(express.urlencoded({ extended: true }));
+server.use(express.static(path.resolve(__dirname, "../public")));
+//sobrescrita de metodo http
+// server.use(methodOverride("_method"));
+server.use(routes);
 
-server.set("view engine", "njk")
+server.set("view engine", "njk");
 
-nunjucks.configure("src/app/views",{
-    express: server,
-    autoescape: false,
-    noCache: true
-})
+nunjucks.configure(path.resolve(__dirname, "app", "views"), {
+  express: server,
+  autoescape: false,
+  noCache: true,
+});
 
-server.listen(5555, function(){
-    console.log('Server is running on the port 5555')
-})
+server.listen(5555, () => console.log("Server is running on the port 5555"));
