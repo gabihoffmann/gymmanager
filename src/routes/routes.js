df1e584bed470fd8a02b9b7d800d7716e8b8c4ed
Gabi Hoffmann <hoffmann.agf@gmail.com>
2021-02-08 07:58:02 -0300

    ♻️  releasing create route to render the form

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -11,10 +11,16 @@ routes.get("/", (req, res) => {
 routes.get("/instructors", (req, res) => {
   res.render("instructors/index");
 });
-//routes.get("/instructors/create", instructors.create);
+routes.get("/instructors/create", (req, res) => {
+  res.render("instructors/create");
+});
+// routes.get("/instructors/create", instructors.create);
 // routes.get('/instructors/:id', instructors.show)
 // routes.get('/instructors/:id/edit', instructors.edit)
 // routes.post('/instructors', instructors.post)
+routes.post("/instructors", (req, res) => {
+  res.send("recebido");
+});
 // routes.put('/instructors', instructors.put)
 // routes.delete('/instructors', instructors.delete)
 //Members
