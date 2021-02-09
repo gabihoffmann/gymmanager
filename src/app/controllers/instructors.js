2e07a247363e3800b585b00b646129690d4fce3c
Gabi Hoffmann <hoffmann.agf@gmail.com>
2021-02-09 08:08:42 -0300

    ♻️  using FS from Node.js to manipulate data and save

--- src/app/controllers/instructors.js
+++ src/app/controllers/instructors.js
@@ -1,6 +1,9 @@
 const Intl = require("intl");
 const { age, date } = require("../../lib/utils");
 
+const fs = require("fs");
+const data = require("../../../data.json");
+
 module.exports = {
   index(req, res) {
     const instructors = data.instructors.map(
@@ -27,7 +30,21 @@ module.exports = {
 
     let { avatarUrl, name, birth, gender, services } = req.body;
 
-    return res.send(req.body);
+    data.instructors.push({
+      id: Number(data.instructors.length + 1),
+      avatarUrl,
+      name,
+      birth: Date.parse(birth),
+      gender,
+      services,
+      created_at: Date.now(),
+    });
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function (error) {
+      if (error) res.send("Write file error!");
+
+      return res.redirect("/instructors");
+    });
   },
 
   show(req, res) {
