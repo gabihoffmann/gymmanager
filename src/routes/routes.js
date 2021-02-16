420e021ad29eaf921ee7b772e0874d136c53caa8
Gabi Hoffmann <hoffmann.agf@gmail.com>
2021-02-16 08:24:18 -0300

    ♻️  releasing the instructor edit route

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -13,7 +13,7 @@ routes.get("/instructors", (req, res) => {
 });
 routes.get("/instructors/create", instructors.create);
 routes.get("/instructors/:id", instructors.show);
-// routes.get('/instructors/:id/edit', instructors.edit)
+routes.get("/instructors/:id/edit", instructors.edit);
 routes.post("/instructors", instructors.post);
 
 // routes.put('/instructors', instructors.put)
